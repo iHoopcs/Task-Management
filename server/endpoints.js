8c5f6019e92b2be11b9055c486022599845cadbf
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2024-07-08 15:44:07 -0400

    inital commit

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,15 +1,43 @@
 const cors  = require('cors'); 
 const express = require('express'); 
 const app = express(); 
+const User = require('./Models/users'); 
+
 app.use(cors({origin: '*'})); 
 app.use(express.json()); 
 
+//GET
 app.get('/', (req, res) => {
     res.send('Welcome to Task Management App!')
 }); 
 
-app.get('/test', (req, res) => {
-    res.send('Test Endpoint')
+app.get('/users', (req, res) => {
+    User.find().then(data => res.status(200).send(data))
+}); 
+
+//POST
+app.post('/create-user', (req, res) => {
+
+    const newUser = User({
+        firstName: 'Caleb', 
+        lastName: 'Simmons', 
+        username: 'admin', 
+        password: 'admin12345', 
+        email: 'caleb.simmons.tm@gmail.com', 
+        role: 'admin',
+        team: '', 
+    })
+
+    newUser.save();
+    res.send('User created'); 
+    console.log(newUser); 
 })
+
+
+//PUT
+
+
+
+//DELETE
 
 module.exports = app; 
--- server/endpoints.test.js
+++ server/endpoints.test.js
@@ -1,9 +1,23 @@
 const request = require('supertest'); 
 const app = require('./endpoints'); 
+const User = require('./Models/users'); 
 
-describe('GET /', () => {
-    test('properly respond with Welcome to Task Management App!', async () => {
-        const response = await request(app).get('/'); 
-        expect(response.statusCode).toBe()
+describe('GET', () => {
+    describe('/', () => {
+        test('properly respond with Welcome to Task Management App!', async () => {
+            const response = await request(app).get('/'); 
+            expect(response.statusCode).toBe(200)
+            expect(response.text).toBe('Welcome to Task Management App!')
+        });
+    }); 
+
+}); 
+
+describe('POST', () => {
+    describe('/create-user', () => {
+        test('properly creates user and adds to database', async () =>{
+            await request(app).post('/create-user').expect(200).expect('User created'); 
+        })
     })
+    
 })
